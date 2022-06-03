4588b5b0717fa5101b18c90d745f49ffc0340e24
kelthemf <favouradigwe@gmail.com>
2022-06-03 16:54:38 -0700

    printalphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Required
+ * Return: 0
+ */
+
+int main(void)
+{
+	char a, A;
+
+	a = 'a';
+	A = 'A';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	while (A <= 'Z')
+	{
+		putchar(A);
+		A++;
+	}
+	putchar('\n');
+	return (0);
+}
