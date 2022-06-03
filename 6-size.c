1339d95c51d120c4b796be0b2710970d7e156f3f
kelthemf <favouradigwe@gmail.com>
2022-06-02 18:04:36 -0700

    Baddest

---
+++ 6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Accesses the program
+ * Return: 0
+ *
+ */
+
+int main(void)
+{  
+	int a;
+	char b;
+	float c;
+	long int d;
+	long long int e;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(b));
+	printf("Size of an int: %zu byte(s)\n", sizeof(a));
+	printf("Size of a long int: %1u byte(s)\n", sizeof(d));
+	printf("Size of a long long int: %1u byte(s)\n", sizeof(e));
+	printf("Size of a float: %zu byte(s)\n", sizeof(c));
+	return (0);
+}
+
