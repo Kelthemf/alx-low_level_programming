c34a6e1be1a2227c53a591fb7efcf7c31a9df948
kelthemf <favouradigwe@gmail.com>
2022-06-08 14:53:11 -0700

    nl3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,14 @@
 void print_alphabet_x10(void)
 {
 	char ch;
-	int i= 0;
+	int i = 0;
 
 	while (i < 10)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
-		}	
+		}
 		i++;
 		_putchar('\n');
 	}
