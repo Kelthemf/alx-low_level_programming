cbdf0ccef5d5dfa76bd40dfbdbe079e637c264a5
kelthemf <favouradigwe@gmail.com>
2022-06-08 14:31:57 -0700

    nloops12

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 -prints the alphabet 10X
+ * print_alphabet_x10 - prints the alphabet 10X
  * Return: 0
  */
 
@@ -9,7 +9,6 @@ void print_alphabet_x10(void)
 {
 	char ch;
 	int i= 0;
-
 
 	while (i < 10)
 	{
