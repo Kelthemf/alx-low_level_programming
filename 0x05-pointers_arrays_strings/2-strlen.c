cde1957338788c71e3a203e6fcc32f75c7d9997e
kelthemf <favouradigwe@gmail.com>
2022-06-14 10:47:52 -0700

    reset98

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be scrutinized
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
