7741fc45f346ca7f0ec588aa6ebf4f2b31384142
kelthemf <favouradigwe@gmail.com>
2022-06-14 10:25:46 -0700

    reset98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes the value of the address in p
+ * to 98
+ * @n: pointer that changes parameter
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
