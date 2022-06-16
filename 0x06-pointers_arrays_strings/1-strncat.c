b08b2d8c8f7f9c0788c282e698fe5c1da3007ab7
kelthemf <favouradigwe@gmail.com>
2022-06-16 01:43:48 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates string a to string b
+ * using only a maximum of n bytes from string b
+ * @dest: origin string
+ * @src: appendage string
+ * @n: number of bytes to be added from one src
+ * Return: address of dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, k;
+
+	k = 0;
+
+	while (dest[k] != '\0')
+	{
+		k++;
+	}
+
+	for (i = 0; dest[i] != '\0' && i < n, i++, k++;)
+	{
+		dest[k] = src[i];
+	}
+
+	dest[k] = '\0';
+
+	return (dest);
+}
