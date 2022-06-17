4aa8f140dfddeb882d0f604fbf568ada52246c20
kelthemf <favouradigwe@gmail.com>
2022-06-17 01:24:12 -0700

    l

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * more_numbers - 0 to 14
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (j < 11)
+	{
+		while (i < 15)
+		{
+			if (i < 10)
+			{
+				_putchar(i + '0');
+			}
+			if (i > 9)
+			{
+				_putchar((i % 10) + '0');
+				_putchar((i / 10) + '0');
+			}
+			i++;
+		}
+		j++;
+	}
+}
